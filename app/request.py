135f947133756051c1f55009af5e51f1c0e68a33
Barnabas27 <bnyambura032@gmail.com>
2020-12-15 00:11:32 +0300

    import requests and json so as to have random quot

--- app/request.py
+++ app/request.py
@@ -1,2 +1,7 @@
-import urllib.request,json
-from .models import Quotes
+import requests,json
+
+def get_random_quotes():
+    r = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    if r.status_code == 200:
+        quote = r.json()
+        return quote
