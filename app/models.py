ab98d38df1c086ca506fac136191078a8dd224b0
Barnabas27 <bnyambura032@gmail.com>
2020-12-15 00:10:45 +0300

    add class and functions

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
 
+
+
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
@@ -13,7 +15,7 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_secure = db.Column(db.String(255))
-    posts = db.relationship('Post',backref = 'user',lazy = 'dynamic')
+    blogs = db.relationship('Blog',backref = 'user',lazy = 'dynamic')
     comment = db.relationship('Comment',backref = 'user',lazy = "dynamic")
     
     @property
@@ -28,8 +30,10 @@ class User(UserMixin,db.Model):
             return check_password_hash(self.password_secure,password)
         
     @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+    def load_user(users_id):
+        return User.query.get(int(users_id))
+        
+    
     
     def __repr__(self):
         return f'User {self.username}'
@@ -43,39 +47,69 @@ class Blog(db.Model):
     content = db.Column(db.Text,nullable=False)
     author = db.Column(db.String(100),nullable=False, default='unknown')
     date_posted = db.Column(db.DateTime,nullable=False, default=datetime.utcnow)
-    
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id',ondelete = 'CASCADE'),nullable = False)
     comments = db.relationship('Comment',backref = 'blog',lazy = 'dynamic')
     
     def save_blog(self):
         db.session.add(self)
         db.session.commit()
         
+    def deleteBlog(self):
+        db.session.delete(self)
+        db.session.commit()
+        
     @classmethod
     def get_blogs(cls,id):
-        blog = Blog.query.filter_by(blog_id = id).all()
-        return blog
+        blogs = Blog.query.filter_by(id = id).all()
+        return blogs
+   
     
-class Quote:
-    def __init__(self,quote):
-        self.quote='http://quotes.stormconsultancy.co.uk/random.json'
-        
+    def __repr__(self):
+        return f'Blogs {self.blog_title}'
+ 
         
 class Comment(db.Model):
     __tablename__ = 'comments'
     
     id = db.Column(db.Integer,primary_key = True)
     comment = db.Column(db.String(500))
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-    blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete = 'CASCADE'))
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id',ondelete = 'CASCADE'))
+    posted = db.Column(db.DateTime, default = datetime.utcnow)
     
     def saveComment(self):
         db.session.add(self)
         db.session.commit()
         
     @classmethod
-    def getComments(cls,blog):
-        comments = Comment.query.filter_by(blog_id = blog).all()
+    def getComments(cls,blog_id):
+        comments = Comment.query.filter_by(blog_id = blog_id).all()
         return comments
     
+    def deleteComment(self):
+        db.session.delete(self)
+        db.session.commit()
     
+    def __repr__(self):
+        return f'User {self.comment}'
+    
+class Subscriber(UserMixin, db.Model):
+   __tablename__="subscribers"
+
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(255))
+   email = db.Column(db.String(255),unique = True,index = True)
+
+
+   def save_subscriber(self):
+       db.session.add(self)
+       db.session.commit()
+
+   @classmethod
+   def get_subscribers(cls,id):
+       return Subscriber.query.all()
+
+
+   def __repr__(self):
+       return f'User {self.email}'
     
